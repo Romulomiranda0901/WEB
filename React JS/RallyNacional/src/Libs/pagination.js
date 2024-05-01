4b3ccdf4bddc2e50e49ebea9fa658ae89bd0b426
RJMF1812 <153560918+RJMF1812@users.noreply.github.com>
2024-04-30 18:16:40 -0600

    react 2

--- React JS/RallyNacional/src/Libs/pagination.js
+++
@@ -1,3 +0,0 @@
- export const fetchDelete = async (data) => {
-
-}
--- React JS/RallyNacional/src/Pages/User.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Listar from "../Components/Usuarios/Listar";
-
-export default function User() {
-  return (
-    <Listar/>
-  )
-}
